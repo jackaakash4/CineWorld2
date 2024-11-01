4976f726350db2450accffdea706ba99d0e9d283
Jack Aakash <jackaakash4@gmail.com>
2024-11-01 13:05:14 +0545

    Feat: Frontend setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Cine World</h1>
+      <p>Your Booking Partner</p>
+      <h2>Let's fucking build this serously</h2>
+    </>
+  )
+}
+
+export default App
